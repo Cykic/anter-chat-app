7bc47f6eb1426084450b278dfb74437485c054b3
Tunji <54449991+Adetunjicode@users.noreply.github.com>
2021-08-12 10:06:31 +0100

    Made som changes on the multer implementaton

--- src/controller/messageController.js
+++ src/controller/messageController.js
@@ -1,7 +1,16 @@
 const catchAsync = require('../error/catchAsync');
 const AppError = require('../error/appError');
 
-const multerStorage = multer.memoryStorage();
+const multerStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/data/uploads');
+    },
+    filename: (req, file, cb) => {
+        const extension = file.mimetype.split('/')[1];
+        // cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+        cb(null, `${file}.image-${Date.now()}.${extension}`); //"image" is the fieldname on the front end form
+    }
+});
 
 const multerFilter = (req, file, cb) => {
     if (file.mimetype.startsWith('image')) {
@@ -16,6 +25,6 @@ const upload = multer({
     fileFilter: multerFilter
 });
 
-exports.sendImage = upload.single('image');
+exports.sendImage = upload.array('image', 12);
 
 exports.createMessage = catchAsync(async (req, res, next) => {});
