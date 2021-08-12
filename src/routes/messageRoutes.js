50850084aa0c2897f09b7e0947390270b497a8db
cykic <cyb3rcykic@gmail.com>
2021-08-12 13:55:33 +0000

    Protect route

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const messageController = require('./../controller/messageController');
+const authController = require('./../controller/authController');
 
-router.use(authController.protect)
+router.use(authController.protect);
 router.post('/', messageController.sendImage, messageController.createMessage);
 
 module.exports = router;
