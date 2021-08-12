2e25ca7ccb906cbd5e65a5c41fea635a6578a34c
cykic <cyb3rcykic@gmail.com>
2021-08-12 11:36:45 +0000

    Feat: Changed verify to GET from POST, error from twilio

--- utils/sendSms.js
+++ utils/sendSms.js
@@ -1,16 +1,22 @@
+const AppError = require('../src/error/appError');
+
 module.exports = function(to, message) {
-    const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-    // console.log(client.api.messages.create())
-    return client.messages
-      .create({
-        body: message,
-        to: to,
-        from: process.env.TWILO_NUMBER,
-      }).then(function(data) {
-        if (process.env.NODE_ENV !== 'production')
-        console.log('ASms Sent', data);
-      }).catch(function(err) {
-        if (process.env.NODE_ENV !== 'production')
-        console.error(err);
-      });
-  };
+  const client = require('twilio')(
+    process.env.TWILIO_ACCOUNT_SID,
+    process.env.TWILIO_AUTH_TOKEN
+  );
+  // console.log(client.api.messages.create())
+  return client.messages
+    .create({
+      body: message,
+      to: to,
+      from: process.env.TWILO_NUMBER
+    })
+    .then(function(data) {
+      console.log('Sms Sent', data);
+    })
+    .catch(function(err) {
+      if (process.env.NODE_ENV !== 'production') console.error(err);
+      console.log('sms failed to send');
+    });
+};
